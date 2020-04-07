6d1cfdd1285089621f09eafef91330b50ad2a718
dan <danlove99@hotmail.co.uk>
2020-04-07 09:47:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Covid19-Severity-Predictor
+[!image](https://www.eud.eu/files/7715/8513/7126/coronavirus-gov.jpg)
+## About
+This is a Tensorflow based application to predict the chance of severity based on your age group.
---
+++ covidSev.py
@@ -0,0 +1,31 @@
+from numpy import loadtxt 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# load the dataset
+dataset = loadtxt('covid.csv', 
+delimiter=',')
+X = dataset[:,0:5] 
+y = dataset[:,5]
+
+# define the keras model
+model = Sequential() 
+model.add(Dense(12, input_dim=5, activation='relu')) 
+model.add(Dense(8, activation='relu')) 
+model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', 
+optimizer='adam', 
+metrics=['accuracy'])
+
+# fit the keras model on the 
+# dataset
+model.fit(X, y, epochs=5, batch_size=10)
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
+
+# predict a 60+ year old
+test = np.array([0,0,0,1])
+model.predict(test)
